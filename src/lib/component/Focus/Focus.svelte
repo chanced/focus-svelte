b111ce58752941cfee4b7a7a46bc8286ecbe5e46
chanced <chanceusc@gmail.com>
2021-09-19 18:13:52 -0400

    removes onMount from Focus component

--- src/lib/component/Focus/Focus.svelte
+++ src/lib/component/Focus/Focus.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-	import { onMount } from "svelte";
 	import { focus } from "$lib/action";
-	let isMounted = false;
-	$: if (isMounted) {
-	}
-	onMount(() => {
-		isMounted = true;
-	});
 	export let enabled: boolean;
 </script>
 
