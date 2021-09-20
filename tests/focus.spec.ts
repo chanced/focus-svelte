d7db168b435b340739315392ed6950b4e2a46215
chanced <chanceusc@gmail.com>
2021-09-20 00:49:00 -0400

    tests are a wip

--- tests/focus.spec.ts
+++ tests/focus.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("focus sets tab indexes appropriately", async ({ page }) => {
 	await page.goto("http://localhost:3000/test");
-	await expect(page).toHaveTitle("test page");
-	await page.locator("input").evaluateAll((nodes) => {
+	const inputs = page.locator("input");
+	await expect(inputs).toHaveCount(5);
+	await inputs.evaluateAll(async (nodes) => {
+		console.log(nodes);
 		for (const node of nodes) {
-			expect(node).toBeInstanceOf(HTMLElement);
-			expect(node.tabIndex).toBe(0);
+			console.log(node);
+			await expect(node.tabIndex).toBe(0);
 		}
-		// this test is failing because:
-		// locator.evaluateAll: Evaluation failed: ReferenceError: _test is not defined
 	});
 });
