4773b4eb276bfa9a1ec9c14f24ae5482ae447568
chanced <chanceusc@gmail.com>
2021-09-27 10:48:35 -0400

    adds focusDelay to the component

--- src/lib/component/Focus/Focus.svelte
+++ src/lib/component/Focus/Focus.svelte
@@ -4,8 +4,9 @@
 	export let assignAriaHidden = false;
 	export let element: HTMLElement | string | undefined = undefined;
 	export let focusable = false;
+	export let focusDelay: number | (() => Promise<void>) | undefined = undefined;
 </script>
 
-<div use:focus={{ enabled, assignAriaHidden, element, focusable }}>
+<div use:focus={{ enabled, assignAriaHidden, element, focusable, focusDelay }}>
 	<slot />
 </div>
